7b7541437028633065988ffa95646686871e7371
santbetv <santiago-betancur@hotmail.com>
2022-11-19 11:11:24 -0500

    Se crea readme

---
+++ README.md
@@ -0,0 +1,26 @@
+# Bot #2 de Maestría
+
+Este es la segunda `práctica de bots`.  Hecho en la [Universidad Autónoma de Manizales](https://www.autonoma.edu.co/).
+
+## Desarrolladores
+
+- Santiago Betancur Villegas
+- Sus grandiosos colaboradores.
+
+## Instalación
+
+```
+$ git clone https://github.com/jimezam/Bot2Maestria.git
+
+$ python -m venv env
+
+$ source env/bin/activate
+
+$ pip -r requirements.txt
+```
+
+## Ejecución
+
+```
+$ python bot.py
+```
--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@
 from config import bot
 import config
 from time import sleep
-import re
+from telebot import types
 
 bot_data = {}
 class Record:
@@ -23,6 +23,85 @@ bot.load_next_step_handlers()
 
 #########################################################
 # Aquí vendrá la implementación de la lógica del bot
+
+@bot.message_handler(commands=['menu'])
+def on_command_menu(message):
+	# Using the ReplyKeyboardMarkup class
+	# It's constructor can take the following optional arguments:
+	# - resize_keyboard: True/False (default False)
+	# - one_time_keyboard: True/False (default False)
+	# - selective: True/False (default False)
+	# - row_width: integer (default 3)
+	# row_width is used in combination with the add() function.
+	# It defines how many buttons are fit on each row before continuing on the next row.
+	markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+	itembtn1 = types.KeyboardButton('/imc')
+	itembtn2 = types.KeyboardButton('/help')
+
+	markup.add(itembtn1, itembtn2)
+	bot.send_message(message.chat.id, "Selecciona una opción del menú:",
+		reply_markup=markup)
+
+
+@bot.message_handler(commands=['imc'])
+def on_command_imc(message):
+	response = bot.reply_to(message, "¿Cuál es tu estatura en metros?")
+	bot.register_next_step_handler(response, process_height_step)
+
+def process_height_step(message):
+	try:
+		height = float(message.text)
+
+		record = Record()
+		record.height = height
+
+		bot_data[message.chat.id] = record
+
+		response = bot.reply_to(message, '¿Cuál es tu peso en kilogramos?')
+
+		bot.register_next_step_handler(response, process_weight_step)
+	except Exception as e:
+		bot.reply_to(message, f"Algo terrible sucedió: {e}")
+
+def process_height_step(message):
+	try:
+		height = float(message.text)
+		record = Record()
+		record.height = height
+		bot_data[message.chat.id] = record
+		response = bot.reply_to(message, '¿Cuál es tu peso en kilogramos?')
+		bot.register_next_step_handler(response, process_weight_step)
+	except Exception as e:
+		bot.reply_to(message, f"Algo terrible sucedió: {e}")
+
+def process_weight_step(message):
+	try:
+		weight = float(message.text)
+		record = bot_data[message.chat.id]
+		record.weight = weight
+		markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+		markup.add('Male', 'Female')
+		response = bot.reply_to(message, '¿Cuál es tu género?',
+		reply_markup=markup)
+		bot.register_next_step_handler(response, process_gender_step)
+	except Exception as e:
+		bot.reply_to(message, f"Algo terrible sucedió: {e}")
+
+def process_gender_step(message):
+	gender = message.text
+	record = bot_data[message.chat.id]
+	record.gender = gender
+	imc(message)
+
+def imc(message):
+	record = bot_data[message.chat.id]
+	imc = record.weight / pow(record.height, 2)
+	answer = f"Data = (Height: {record.height}, Weight: {record.weight}, Gender: {record.gender})\nIMC = {imc}\nDaticos {validarPeso(imc)}"
+	bot.reply_to(message, answer)
+
+def validarPeso(imc):
+	if (imc > 0):
+		return "datos"
 
 @bot.message_handler(func=lambda message: True)
 def on_fallback(message):
